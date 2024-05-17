a45fada553a9209e1f5a29470d8bbd6f9f7ead45
UnexomWid <uw@exom.dev>
2024-05-17 17:26:14 +0300

    fix: setup

--- setup.bat
+++ setup.bat
@@ -1,13 +1,21 @@
 @echo OFF
 
+echo "Setting up server..."
+
 cd server
 
-npm i
+call npm i
+
+echo "Copying config..."
 
 copy config.sample.json config.json
+
+echo "Setting up client..."
 
 cd ../client
 
 cargo run -- --setup
 
 cd ..
+
+echo "All done!"
