9f602c037fc45d46fcee3c4b835be9c5bb733dee
UnexomWid <uw@exom.dev>
2024-05-20 17:12:50 +0300

    add: auto admin elevation

--- start.bat
+++ start.bat
@@ -1,3 +1,13 @@
 @echo OFF
 
+net session >nul 2>&1
+if %errorlevel% == 0 (
+    goto :run
+)
+
+echo WAID needs to be ran as admin, requesting permissions...
+powershell start cmd.exe -verb runas -ArgumentList '/c', 'cd /d "%cd%" ^&^& "%0"' & exit /b
+
+:run
+
 wt ; new-tab -p "Command Prompt" -d "server" --title "WAID Server" cmd.exe /k npm start ; new-tab -p "Command Prompt" -d "client" --title "WAIT Client" cmd.exe /k cargo run --release
